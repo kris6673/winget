37572ca472fd1fba0364b806defbdefc365c772a
Kristian Kjærgård <kris6673@hotmail.com>
2025-03-25 00:00:12 +0100

    Multi upload template

---
+++ MultiUploadCommand.ps1
@@ -0,0 +1,11 @@
+﻿$Versions = @(
+    '0.6.00'
+    '0.6.01'
+)
+$PackageName = 'LightBurnSoftware.LightBurn'
+
+foreach ($Version in $Versions) {
+    
+    $DownloadUrl = 'https://release.lightburnsoftware.com/LightBurn/Release/LightBurn-v{0}/LightBurn-v{0}.exe' -f $Version
+    komac update $PackageName -v $Version -u $DownloadUrl --submit --dry-run
+}
